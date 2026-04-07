22436667d2599df2969dec70bf2f9900e8b0ba19
Timmy <timmy@comet.la>
2026-04-08 06:09:32 +0700

    dunno-workflow 0.1.2: update sha256 for version bump

--- Formula/dunno-workflow.rb
+++ Formula/dunno-workflow.rb
@@ -2,7 +2,7 @@ class DunnoWorkflow < Formula
   desc "Declarative Claude Code agent team orchestration for iTerm2"
   homepage "https://github.com/cometdigitalagency/dunno-workflow"
   url "https://github.com/cometdigitalagency/dunno-workflow/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "c29437304b4d41fd42fb7263b51e7320d2c4cc7b448421c70c0c740dd6d5d2cd"
+  sha256 "dbdf811ccda8373cc64e231721d55661afefc75a4eb2166a93858204830d6356"
   license "MIT"
 
   depends_on "yq"
