4cc0d3966ff8694a39feff40c873d726b23b07d7
Arnaud <arnaud.phommasone@gmail.com>
2026-04-12 20:43:23 +0700

    bump dunno-workflow to v0.3.8

--- Formula/dunno-workflow.rb
+++ Formula/dunno-workflow.rb
@@ -1,8 +1,8 @@
 class DunnoWorkflow < Formula
   desc "Declarative Claude Code agent team orchestration for iTerm2"
   homepage "https://github.com/cometdigitalagency/dunno-workflow"
-  url "https://github.com/cometdigitalagency/dunno-workflow/archive/refs/tags/v0.3.7.tar.gz"
-  sha256 "ef07f7362288c6db10d1d2f2c608820251c813f2313811d816450f7c38cc0b04"
+  url "https://github.com/cometdigitalagency/dunno-workflow/archive/refs/tags/v0.3.8.tar.gz"
+  sha256 "abb114f14a597058271f7c1323d4531069572fcdb90958fc6eaa26ad45a4fbfd"
   license "MIT"
 
   depends_on "yq"
